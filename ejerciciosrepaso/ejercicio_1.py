2bbef870b74035fe7b94825d958d0a92dc10dace
Steven <jhonsteven.163@gmail.com>
2023-08-01 21:36:51 -0500

    Commit 1

---
+++ ejerciciosrepaso/ejercicio_1.py
@@ -0,0 +1,10 @@
+def ingresar_datos():
+    nombre= input("Ingrese su nombre:")
+    edad= input("Ingrese su edad:")
+
+    print("Tu nombre es:", nombre)
+    print("Tu edad es", edad)
+
+if __name__ == "__main__":
+    ingresar_datos()
+
