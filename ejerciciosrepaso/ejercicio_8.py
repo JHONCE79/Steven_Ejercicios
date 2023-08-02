f11c90fd02db075c49576eabf1252b448a195989
Steven <jhonsteven.163@gmail.com>
2023-08-01 21:39:16 -0500

    Commit 8

---
+++ ejerciciosrepaso/ejercicio_8.py
@@ -0,0 +1,9 @@
+def invertir(lista):
+    lista_invertida = lista[::-1]
+    return lista_invertida
+
+# Ejemplo de uso
+lista_original = [1, 2, 3, 4, 5]
+lista_invertida = invertir(lista_original)
+print("Lista original:", lista_original)
+print("Lista invertida:", lista_invertida)
