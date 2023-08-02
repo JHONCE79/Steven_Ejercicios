a11e3b98bb7f8f7d20432f7defd558a74cb3fb73
Steven <jhonsteven.163@gmail.com>
2023-08-01 21:36:26 -0500

    Commit 6

---
+++ ejerciciosrepaso/ejercicio_6.py
@@ -0,0 +1,13 @@
+def suma_lista(lista):
+    suma = 0
+    for numero in lista:
+        suma += numero
+    return suma
+
+# Ejemplo de uso
+lista_numeros = [1,2,3,4,5]
+resultado_suma = suma_lista(lista_numeros)
+print(f"La suma de los números en la lista {lista_numeros} es: {resultado_suma}")
+
+
+
